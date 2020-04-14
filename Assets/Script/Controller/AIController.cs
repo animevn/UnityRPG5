7e37e7c1faf20af737d9b3568e03e75151f31ff7
animevn <haanhth21@gmail.com>
2020-04-14 12:00:21 +0700

    Part 2 ok

---
+++ Assets/Script/Controller/AIController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Script.Controller
+{
+    // ReSharper disable once InconsistentNaming
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] private float chaseDistance = 10f;
+
+        private float DistanceToPlayer()
+        {
+            var player = GameObject.FindWithTag("Player");
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
+
+        private void Update()
+        {
+            if (DistanceToPlayer() < chaseDistance)
+            {
+                print("Should chase now");
+            }
+        }
+    }
+}
