faa5a37c379fe3ba4aa4a3c222601edf02910f4e
animevn <haanhth21@gmail.com>
2020-04-14 18:53:28 +0700

    Part 8 suspicion time ok

--- Assets/Script/Controller/AIController.cs
+++ Assets/Script/Controller/AIController.cs
@@ -10,15 +10,19 @@ namespace Script.Controller
     public class AIController : MonoBehaviour
     {
         [SerializeField] private float chaseDistance = 10f;
+        [SerializeField] private float suspisionTime = 2f;
         private Fighter fighter;
         private Health health;
+        private Mover mover;
         private GameObject player;
         private Vector3 guardPosition;
+        private float timeSinceLastSawPlayer;
 
         private void Start()
         {
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
             player = GameObject.FindWithTag("Player");
             guardPosition = transform.position;
         }
@@ -33,14 +37,19 @@ namespace Script.Controller
             if (health.IsDead()) return;
             if (InAttackRange() && fighter.CanAttack(player))
             {
+                timeSinceLastSawPlayer = 0;
                 fighter.Attack(player);
+            }
+            else if (timeSinceLastSawPlayer < suspisionTime)
+            {
+                GetComponent<ActionScheduler>().CancelAction();
             }
             else
             {
                 fighter.Cancel();
-                GetComponent<Mover>().StartToMoveTo(guardPosition);
+                mover.StartToMoveTo(guardPosition);
             }
-            
+            timeSinceLastSawPlayer += Time.deltaTime;
         }
 
         private void OnDrawGizmos()
