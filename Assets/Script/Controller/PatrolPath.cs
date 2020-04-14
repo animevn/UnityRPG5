74bc42658d8c0d9cd42ce06723430e1afc0e88e5
animevn <haanhth21@gmail.com>
2020-04-14 19:47:53 +0700

    Part 9 ok now

---
+++ Assets/Script/Controller/PatrolPath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script.Controller
+{
+    public class PatrolPath: MonoBehaviour
+    {
+        [SerializeField] private float waypointRadius = 0.2f;
+        
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(transform.GetChild(i).position, waypointRadius);  
+            }
+        }
+    }
+}
